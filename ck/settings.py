1e136d600c6e1cd349807bb1e989c8b7b4efd896
Mansi Kamthan <mkamthan@unomaha.edu>
2023-04-15 23:16:34 -0500

    Update settings.py

--- ck/settings.py
+++ ck/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_#ymoe79&8odlpv2n6f!urz2-(y0w5yd3t%!f9!f+&%6h#x+ag'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
