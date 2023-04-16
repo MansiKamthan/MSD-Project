d00d1f6fccbe86dbcc4d8a8d134f509718b25079
Mansi Kamthan <mkamthan@unomaha.edu>
2023-04-15 23:05:58 -0500

    First Commit

---
+++ home/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, re_path
+from .views import login_view, logout_view, register_view, home_view
+from django.conf import settings
+from django.conf.urls.static import static
+
+app_name = 'home'
+
+urlpatterns = [
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('register/', register_view, name='register'),
+    path('', home_view, name='home'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
