f3aea7e6e7731bb68a497bae523f02f7fdb47e55
srujan68 <skunduru@unomaha.edu>
2023-04-21 09:23:30 -0500

    Updated settings

--- ck/settings.py
+++ ck/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_#ymoe79&8odlpv2n6f!urz2-(y0w5yd3t%!f9!f+&%6h#x+ag'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
